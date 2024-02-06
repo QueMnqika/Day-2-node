57d0a34912a5a32d53dca52dea96887b86a872ff
Que Mnqika <qaqambilemnqika23@gmail.com>
2024-02-06 15:03:48 +0200

    added a writeFILE, readFile, appendFile

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 let fs = require('fs')
 let path = require('path')
-let fileName = path.join(__dirname, './data/data.txt')
+let fileName = path.join(__dirname, './data/dataUn.txt')
 
-function writeFile(content) {
+
+function writeFromFile(content) {
     try {
-        fs.writeFile(fileName, content, err => {
+        fs.writeFile(fileName, content, (err) => {
             if (!err) {
                 console.log('Saved');
             }
@@ -13,3 +14,28 @@ function writeFile(content) {
         console.log('error when writing');
     }   
 }
+
+
+function readFromFile() {
+    fs.readFile(file, Name, 'utf8', (err, data) => {
+        if (!err) {
+            console.log(data);
+        }
+    })
+}
+
+function AppendToFile(content) {
+    try {
+        fs.appendFile(fileName, content, (err) => {
+            if (!err) {
+                console.log('Data is modified');
+            }
+        })
+    } catch (e) {
+        console.log('Issue appending your data to file');
+    }
+}
+
+writeFromFile('First content')
+// writeFile('Second content')
+// writeFile('First content')
