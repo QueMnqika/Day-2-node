3258d0ea9f25c9d0d3fa376ec190e09181a94acc
Que Mnqika <qaqambilemnqika23@gmail.com>
2024-02-06 14:46:14 +0200

    add 2

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+let fs = require('fs')
+let path = require('path')
+let fileName = path.join(__dirname, './data/data.txt')
 
-let data = require('./data/data.txt')
-
+function writeFile(content) {
+    try {
+        fs.writeFile(fileName, content, err => {
+            if (!err) {
+                console.log('Saved');
+            }
+        })
+    } catch (e) {
+        console.log('error when writing');
+    }   
+}
